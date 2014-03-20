50046906ec2c1a4a44c7be90aed8110357efcd9c
hatten33 <hatten33@gmail.com>
2014-03-19 18:31:42 -0700

    merged command functionality with NPManager

--- src/net/aerenserve/networkpoints/NetworkPoints.java
+++ src/net/aerenserve/networkpoints/NetworkPoints.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 
 import net.aerenserve.minesql.MineSQL;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -16,7 +17,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class NetworkPoints extends JavaPlugin implements Listener {
 	
-	MineSQL minesql;
+	static MineSQL minesql;
+	static String VERSION = "1.4";
 	
 	@Override
 	public void onEnable() {
@@ -39,21 +41,53 @@ public class NetworkPoints extends JavaPlugin implements Listener {
 			e.printStackTrace();
 		}
 		
-		getLogger().info("NetworkPoints v1.3 by hatten33 enabled");
+		getLogger().info("NetworkPoints v" + VERSION + " by hatten33 enabled");
 	}
 	
 	@Override
 	public void onDisable() {
-		getLogger().info("NetworkPoints v1.3 by hatten33 disabled");
+		getLogger().info("NetworkPoints v" + VERSION + " by hatten33 disabled");
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(sender instanceof Player) {
 			Player p = (Player) sender;
-			if(cmd.getName().equalsIgnoreCase("mypoints")) {
-				p.sendMessage(ChatColor.GRAY + "You currently have " + ChatColor.GREEN + getBalance(p.getName()) + ChatColor.GRAY + " points.");
+			if(cmd.getName().equalsIgnoreCase("points")) {
+				if(args.length >= 1) {
+					if(args[0].equalsIgnoreCase("get")) {
+						String account = null;
+						if(sender instanceof Player) account = sender.getName();
+						if(args.length > 1 && Bukkit.getPlayer(args[1]) != null) account = args[1];
+						if(account != null) {
+							sender.sendMessage(ChatColor.GRAY + "You currently have " + ChatColor.GREEN + getBalance(p.getName()) + ChatColor.GRAY + " points.");
+						} else sender.sendMessage("You must be a player to see your balance");
+					}
+					if(args[0].equalsIgnoreCase("add")) {
+						if(args.length >= 3) {
+							String account = args[1];
+							Integer amount = Integer.parseInt(args[2]);
+							addPoints(account, amount);
+						} else sender.sendMessage(ChatColor.RED + "Usage: /points add (player) (amount)");
+					}
+					if(args[0].equalsIgnoreCase("subtract")) {
+						if(args.length >= 3) {
+							String account = args[1];
+							Integer amount = Integer.parseInt(args[2]);
+							removePoints(account, amount);
+						} else sender.sendMessage(ChatColor.RED + "Usage: /points subtract (player) (amount)");
+					}
+					if(args[0].equalsIgnoreCase("set")) {
+						if(args.length >= 3) {
+							String account = args[1];
+							Integer amount = Integer.parseInt(args[2]);
+							setBalance(account, amount);
+						} else sender.sendMessage(ChatColor.RED + "Usage: /points set (player) (amount)");
+					}
+					
+				} else sender.sendMessage(ChatColor.RED + "Usage: /points (get | add | subtract | set)");
 			}
+			return false;
 		}
 		return false;
 	}
@@ -63,7 +97,7 @@ public class NetworkPoints extends JavaPlugin implements Listener {
 		createPlayer(e.getPlayer().getName());
 	}
 	
-	public void createPlayer(String playername) {
+	public static void createPlayer(String playername) {
 		if(!playerExists(playername)) {
 			try {
 				minesql.updateSQL("INSERT INTO playerpoints (username, balance) VALUES ('" + playername + "',0);");
@@ -73,7 +107,7 @@ public class NetworkPoints extends JavaPlugin implements Listener {
 		}
 	}
 	
-	public boolean playerExists(String playername) {
+	public static boolean playerExists(String playername) {
 		try {
 			ResultSet res = minesql.querySQL("SELECT * FROM playerpoints WHERE username = '" + playername + "';");
 			if(res.next()) {
@@ -91,27 +125,33 @@ public class NetworkPoints extends JavaPlugin implements Listener {
 		return false;
 	}
 	
-	public boolean checkTransaction(String playername, Integer amount) {
+	public static boolean checkTransaction(String playername, Integer amount) {
 		if(getBalance(playername) >= amount) return true;
 		else return false;
 	}
 	
-	public Integer getBalance(String playername) {
+	public static Integer getBalance(String playername) {
 		Integer retval = null;
-		try {
-			ResultSet res = minesql.querySQL("SELECT * FROM playerpoints WHERE username = '" + playername + "';");
-			if(res.next()) {
-				if((Integer) res.getInt("balance") != null) {
-					retval = res.getInt("balance");
+		if(playerExists(playername)) {
+			try {
+				ResultSet res = minesql.querySQL("SELECT * FROM playerpoints WHERE username = '" + playername + "';");
+				if(res.next()) {
+					if((Integer) res.getInt("balance") != null) {
+						retval = res.getInt("balance");
+					}
 				}
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} else {
+			createPlayer(playername);
+			getBalance(playername);
 		}
 		return retval;
+		
 	}
 	
-	public void addPoints(String playername, Integer amount) {
+	public static void addPoints(String playername, Integer amount) {
 		if(playerExists(playername)) {
 			setBalance(playername, (getBalance(playername) + amount));
 		} else {
@@ -120,7 +160,7 @@ public class NetworkPoints extends JavaPlugin implements Listener {
 		}
 	}
 	
-	public void setBalance(String playername, Integer amount) {
+	public static void setBalance(String playername, Integer amount) {
 		if(playerExists(playername)) {
 			try {
 				minesql.updateSQL("UPDATE playerpoints SET balance=" + amount + " WHERE username='" + playername + "';");
@@ -133,7 +173,7 @@ public class NetworkPoints extends JavaPlugin implements Listener {
 		}
 	}
 	
-	public void removePoints(String playername, Integer amount) { //TODO add special exceptions
+	public static void removePoints(String playername, Integer amount) { //TODO add special exceptions
 		if(playerExists(playername)) {
 			if(checkTransaction(playername, amount)) {
 				setBalance(playername, (getBalance(playername) - amount));
